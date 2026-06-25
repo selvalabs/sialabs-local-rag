e4f87f75e978c3c83ab4da0b60a9cb31bec1ec7f
mtv-astro <mtv-astro@proton.me>
2026-06-25 12:28:16 -0300

    feat(frontend): render local launcher panel

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,14 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App'
+import { LauncherPanel } from './LauncherPanel'
 import { registerServiceWorker } from './pwa'
 import './styles.css'
 import './workspace.css'
+import './launcher.css'
 
 registerServiceWorker()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <LauncherPanel />
   </React.StrictMode>,
 )
